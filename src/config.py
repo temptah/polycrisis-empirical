201c9f61f16876cab3a7ebab2720b83a7c5fd36f
sofoklis papakonstantinou <sofoklispapakonstantinou@yahoo.gr>
2025-09-02 20:56:26 +0300

    Config: central DB engine helper

---
+++ src/config.py
@@ -0,0 +1,15 @@
+﻿import os
+from dotenv import load_dotenv
+import sqlalchemy as sa
+
+def make_engine():
+    load_dotenv()
+    url = sa.URL.create(
+        "postgresql+psycopg2",
+        username=os.getenv("DB_USER", "postgres"),
+        password=os.getenv("DB_PASSWORD", "postgres"),
+        host=os.getenv("DB_HOST", "localhost"),
+        port=int(os.getenv("DB_PORT", "5432")),
+        database=os.getenv("DB_NAME", "postgres"),
+    )
+    return sa.create_engine(url, future=True)
